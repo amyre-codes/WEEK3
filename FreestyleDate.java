4ce4f5dec31f72ae68e5dce1e06a8892ba4b5544
amyre-codes <52667955+amyre-codes@users.noreply.github.com>
2019-07-30 13:59:56 -0400

    Add files via upload

---
+++ FreestyleDate.java
@@ -0,0 +1,32 @@
+package date1;
+
+import java.util.Scanner;
+
+public class FreestyleDate {
+
+	public static void main(String[] args) {
+		
+		Scanner apple = new Scanner(System.in);
+		int five = 25;
+		String answer = "";
+	String bowling = "go bowling";
+	String  movies= "go see a movie";
+	String camping = "go camping";
+	String concert = "go to a concert";
+	
+	
+		System.out.println("enter your name");
+				answer = apple.next();
+System.out.println("do you like dates indoors or outdoors " + answer);
+         answer = apple.next();
+           
+            
+        
+        
+
+}
+		
+
+	}
+
+
